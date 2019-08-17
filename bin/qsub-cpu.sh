f2e86eddda9ad1e4ab11eb7df4f0587a7d42d822
alecokas <alecokastanos@gmail.com>
2019-08-17 09:11:22 +0100

    Remove bins and include PR curves for grapheme features

--- bin/qsub-cpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 script.sh output.log"
-fi
-SCRIPT=$1
-LOG=$2
-
-qsub -cwd -S /bin/bash -j y -P black-svr -l qp="low" -l mem_grab=4G -l mem_free=4G -pe smp 8 -o $LOG $SCRIPT
--- bin/qsub-gpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 script.sh output.log"
-fi
-SCRIPT=$1
-LOG=$2
-
-qsub -cwd -S /bin/bash -j y -P black-svr -l qp="cuda-low" -o $LOG $SCRIPT
