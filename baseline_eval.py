554640fb2218864e54cac669c433af6e7ddb5ea2
alecokas <alecokastanos@gmail.com>
2019-08-17 13:53:39 +0100

    Docstring update

--- baseline_eval.py
+++ baseline_eval.py
@@ -7,6 +7,12 @@ import utils
 
 
 def pred_ref_lists(file_split, target_dir):
+    """ Obtain the predictions and reference confidence score lists.
+
+        Arguments:
+            file_split: The file containing the paths to processed lattices in the test set
+            target_dir: Name of the target directory
+    """
     with open(file_split, 'r') as test_split_file:
         lattice_path_list = []
         target_path_list = []
@@ -25,12 +31,21 @@ def load_ref(path):
     a = np.load(path)
     return a['ref'].tolist(), a['indices'].tolist()
 
-def load_pred(path, indices):
-    """ Load the posterior predictions for the one-best. """
+def load_pred(path, indices, post_idx=-1):
+    """ Load the posterior predictions for the one-best.
+        By default we assume that the posteriors are stored in the final column.
+
+        Arguments:
+            path: Path to processed lattice file (*.npz)
+            indices: The arc indices to obtain posteriors for
+            post_idx: Posterior column index
+    """
     a = np.load(path)
-    return a['edge_data'][indices,-1].tolist()
+    return a['edge_data'][indices, post_idx].tolist()
 
 def load_eval_data(lattice_path_list, target_path_list):
+    """ Generate a numpy array of predictions and an array of references.
+    """
     preds = []
     refs = []
     for pred_path, ref_path in zip(lattice_path_list, target_path_list):
@@ -42,14 +57,12 @@ def load_eval_data(lattice_path_list, target_path_list):
 
 def main(args):
     target_dir = os.path.join(args.root, args.target_dir)
-    split_file = os.path.join(args.root, 'test.txt')
-    lattice_path_list, target_path_list = pred_ref_lists(split_file, target_dir)
+    test_split_file = os.path.join(args.root, 'test.txt')
+    lattice_path_list, target_path_list = pred_ref_lists(test_split_file, target_dir)
     preds, refs = load_eval_data(lattice_path_list, target_path_list)
     assert len(preds) == len(refs), \
          'Predictions and references must be sequences of the same length'
 
-    print(preds)
-    print(refs)
     nce = evaluation.nce(refs, preds)
     precision, recall, area = evaluation.pr(refs, preds)
     precision_bl, recall_bl, area_bl = evaluation.pr(refs, preds)
@@ -65,9 +78,8 @@ def main(args):
 
 
 def parse_arguments(args_to_parse):
-    """ Parse the command line arguments.
-    """
-    description = "Run evaluation on the one-best baseline numbers"
+    """ Parse the command line arguments. """
+    description = "Run evaluation on the one-best baseline numbers for word posterior confidence estimator. "
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument('-r', '--root', required=True, type=str,
